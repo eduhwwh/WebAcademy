f5ecf7ac680a9c59737eb94fed70994f0678eabc
maria Eduarda <vantev806@gmail.com>
2025-07-08 19:11:13 -0500

    exemplo de JavaScript

--- Introducao-Fundamentos-Front_End/js/script.js
+++ Introducao-Fundamentos-Front_End/js/script.js
@@ -1,1 +1,20 @@
-console.log("Página carregada com sucesso!");
+let btn = document.querySelector('button');
+
+btn.onclick = () =>{
+    alert('Forma externa na tg HTML.');
+}
+
+//Escutador de eventos
+
+btn.addEventListener('click', exibirMensagem);
+btn.addEventListener('click',alterarTitulo);
+
+function alterarTitulo(){
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'WebAcademy';
+    titulo.style.color = 'green'
+}
+
+function exibirMensagem(){
+    alert('Forma de eventos do JavaScript');
+}
