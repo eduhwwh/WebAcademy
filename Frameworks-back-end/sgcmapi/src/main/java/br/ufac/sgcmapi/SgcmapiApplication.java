dee78af9345cf17e5874a691a985b82b51c034a2
Maria Eduarda <vantev806@gmail.com>
2025-07-30 19:10:58 -0500

    Adicionando uma dempendecia no pom

--- Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
+++ Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/SgcmapiApplication.java
@@ -2,6 +2,7 @@ package br.ufac.sgcmapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SgcmapiApplication {
 
+	private ExemploService exemploService;
+
+	public SgcmapiApplication(ExemploService exemploService){
+		this.exemploService = exemploService;
+	}
+
 	@RequestMapping(value = "/")
 	public String exemplo() {
-		return "teste";
+		// return "SGCM";
+
+		return exemploService.exibirMensagem();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgcmapiApplication.class, args);
+	}
+
+	@Service
+	public static class ExemploService {
+
+		public String exibirMensagem(){
+
+			return "SGCM funcionando";
+
+		}
+		
 	}
 
 }
