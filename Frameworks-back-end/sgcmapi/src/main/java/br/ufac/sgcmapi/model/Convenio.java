bd90d10442f66d06e4d7a61a83cb1cf9c875cb5a
Maria Eduarda <vantev806@gmail.com>
2025-07-31 18:20:15 -0500

    Resolução da atividade

--- Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/model/Convenio.java
+++ Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/model/Convenio.java
@@ -2,15 +2,39 @@ package br.ufac.sgcmapi.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Convenio implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String razaoSocial;
+
+    @Column(nullable = false)
     private String cnpj;
+
+    @Column(nullable = false)
     private String representante;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String telefone;
+
+    @Column(nullable = false)
     private boolean ativo = true;
     
     public Long getId() {
--- Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/model/Unidade.java
+++ Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/model/Unidade.java
@@ -2,12 +2,25 @@ package br.ufac.sgcmapi.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Unidade implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
     private Long id;
+
+    @Column(nullable = false, unique =  true)
     private String nome;
-    private String endereco;
     
+    private String endereco;
+
     public Long getId() {
         return id;
     }
