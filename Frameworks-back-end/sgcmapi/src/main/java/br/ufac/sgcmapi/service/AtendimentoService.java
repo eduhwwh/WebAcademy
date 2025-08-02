cc40d2c8cc31c0041fbfa364c2f154dc7d791ca3
Maria Eduarda <vantev806@gmail.com>
2025-08-01 19:05:52 -0500

    Criando Services

---
+++ Frameworks-back-end/sgcmapi/src/main/java/br/ufac/sgcmapi/service/AtendimentoService.java
@@ -0,0 +1,54 @@
+package br.ufac.sgcmapi.service;
+
+import java.util.List;
+
+import br.ufac.sgcmapi.model.Atendimento;
+import br.ufac.sgcmapi.model.EStatus;
+import br.ufac.sgcmapi.repository.AtendimentoRepository;
+
+public class AtendimentoService implements ICrudService<Atendimento>{
+
+
+    
+    private final AtendimentoRepository repo;
+
+    
+    public AtendimentoService(AtendimentoRepository repo){
+        this.repo = repo;
+    }
+    
+
+    @Override
+    public List<Atendimento> consultar(String termoBuscar) {
+        
+        return repo.findAll();
+
+    }
+
+    @Override
+    public Atendimento consultar(Long id) {
+        
+        return repo.findById(id).orElse(null);
+
+    }
+
+    @Override
+    public Atendimento salvar(Atendimento objeto) {
+
+        return repo.save(objeto);
+
+    }
+
+    @Override
+    public void remover(Long id) {
+
+        var registro = this.consultar(id);
+        if(registro != null){
+            registro.setStatus(EStatus.CANCELADO);
+            repo.save(registro);
+        }
+
+
+    }
+    
+}
