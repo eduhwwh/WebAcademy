f7d5eab4faed8b955875a93ba898ff8ae3f65f0b
Maria Eduarda <vantev806@gmail.com>
2025-08-07 20:25:50 -0500

    Aula 07082025

--- frameworks-front-end-t7/sgcmapp/src/app/app.routes.ts
+++ frameworks-front-end-t7/sgcmapp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AgendaListComponent } from './component/agenda/agenda-list/agenda-list.component';
+import { AgendaFormComponent } from './component/agenda/agenda-form/agenda-form.component';
+import { AtendimentoComponent } from './component/atendimento/atendimento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'agenda-list', component: AgendaListComponent},
+    {path: 'agenda-form', component: AgendaFormComponent},
+    {path: 'atendimento', component: AtendimentoComponent}
+
+
+];
