f350f0692c9af2206c656ce5ce01e036c4616ab0
Maria Eduarda <vantev806@gmail.com>
2025-08-08 18:58:19 -0500

    Adiconando interfaces ICrud

--- frameworks-front-end-t7/sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.ts
+++ frameworks-front-end-t7/sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Atendimento } from '../../../model/atendimento';
+import { ICrudForm } from '../../i-crud-form';
 
 @Component({
   selector: 'app-agenda-form',
@@ -6,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './agenda-form.component.html',
   styles: ``
 })
-export class AgendaFormComponent {
+export class AgendaFormComponent implements ICrudForm<Atendimento> {
+
+  registro: Atendimento = <Atendimento>{};
+
+  salvar(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- frameworks-front-end-t7/sgcmapp/src/app/component/agenda/agenda-list/agenda-list.component.ts
+++ frameworks-front-end-t7/sgcmapp/src/app/component/agenda/agenda-list/agenda-list.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Atendimento } from '../../../model/atendimento';
+import { ICrudList } from '../../i-crud-list';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agenda-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './agenda-list.component.html',
   styles: ``
 })
-export class AgendaListComponent {
+export class AgendaListComponent implements ICrudList<Atendimento> {
+
+  registros: Atendimento[] = [];
+
+  consultar(termoBusca?: string): void {
+    throw new Error('Method not implemented.');
+  }
+
+  remover(id: number): void {
+    console.log('Remover: ', id);
+  }
 
 }
