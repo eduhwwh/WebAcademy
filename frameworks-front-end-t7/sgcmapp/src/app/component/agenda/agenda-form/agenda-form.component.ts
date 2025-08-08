af99fd2c2fc5946c8da41406ab36ae79179abbfa
Maria Eduarda <vantev806@gmail.com>
2025-08-07 20:04:39 -0500

    Criando os componentes

---
+++ frameworks-front-end-t7/sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda-form',
+  imports: [],
+  templateUrl: './agenda-form.component.html',
+  styles: ``
+})
+export class AgendaFormComponent {
+
+}
---
+++ frameworks-front-end-t7/sgcmapp/src/app/component/atendimento/atendimento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atendimento',
+  imports: [],
+  templateUrl: './atendimento.component.html',
+  styles: ``
+})
+export class AtendimentoComponent {
+
+}
