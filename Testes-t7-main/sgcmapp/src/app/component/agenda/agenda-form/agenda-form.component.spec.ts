6055fbe1a645621fe0609b25c04a61f2cbab09b1
Maria Eduarda <vantev806@gmail.com>
2025-08-19 19:53:03 -0300

    Refs.: Criando teste no Agenda-form, utilizando Jasmine

--- Testes-t7-main/sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.spec.ts
+++ Testes-t7-main/sgcmapp/src/app/component/agenda/agenda-form/agenda-form.component.spec.ts
@@ -4,17 +4,27 @@ import { provideHttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { routes } from '../../../app.routes';
 import { AgendaFormComponent } from './agenda-form.component';
+import mockConvenios from '../../../../assets/json/convenios.json'
+import { of } from 'rxjs';
+import { ConvenioService } from '../../../service/convenio.service';
 
 describe('AgendaFormComponent', () => {
   let component: AgendaFormComponent;
   let fixture: ComponentFixture<AgendaFormComponent>;
+
+  const servicoConvenio = {
+    consultarAtivos: jasmine.createSpy('consultarAtivos').and.returnValue(of(mockConvenios))
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         AgendaFormComponent,
         RouterModule.forRoot(routes)],
-      providers: [provideHttpClient()]
+      providers: [
+        provideHttpClient(),{
+          provide: ConvenioService, useValues: servicoConvenio
+        }]
     })
     .compileComponents();
 
@@ -25,5 +35,16 @@ describe('AgendaFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve listar as opções de convenio no formulario ao iniciar', ()=> {
+    component.ngOnInit();
+    fixture.detectChanges();
+    const select = fixture.nativeElement.querySelector('select#convenio');
+    
+    expect(select).not.toBeNull();
+    mockConvenios.forEach(
+      convenio => expect(select.innerHTML).toContain.(convenio.nome)
+    );
   });
 });
--- Testes-t7-main/sgcmapp/src/app/service/atendimento.service.spec.ts
+++ Testes-t7-main/sgcmapp/src/app/service/atendimento.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AtendimentoService } from './atendimento.service';
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
