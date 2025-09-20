ff1d1ebef9a46a9758d43db0d4c65e06d7b9a480
Maria Eduarda <vantev806@gmail.com>
2025-09-19 21:40:27 -0300

    feat: validação no front-end

--- Programacao-Avancada-Front-End-t7-main/sgcmapp/src/app/component/profissional/profissional-form/profissional-form.component.ts
+++ Programacao-Avancada-Front-End-t7-main/sgcmapp/src/app/component/profissional/profissional-form/profissional-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { Especialidade } from '../../../model/especialidade';
 import { Profissional } from '../../../model/profissional';
@@ -8,11 +8,11 @@ import { EspecialidadeService } from '../../../service/especialidade.service';
 import { ProfissionalService } from '../../../service/profissional.service';
 import { UnidadeService } from '../../../service/unidade.service';
 import { ICrudForm } from '../../i-crud-form';
-import { NgClass } from "../../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profissional-form',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './profissional-form.component.html',
   styles: ``
 })
@@ -36,7 +36,10 @@ export class ProfissionalFormComponent implements ICrudForm<Profissional>, OnIni
     const id = this.rota.snapshot.queryParamMap.get('id');
     if (id) {
       this.servico.consultarPorId(+id).subscribe({
-        next: resposta => this.registro = resposta
+        next: resposta => {
+          this.registro = resposta;
+          this.formProfissional.patchValue(this.registro);
+        }
       });
     }
   }
@@ -44,8 +47,22 @@ export class ProfissionalFormComponent implements ICrudForm<Profissional>, OnIni
   registro: Profissional = <Profissional>{};
   especialidades: Especialidade[] = [];
   unidades: Unidade[] = [];
+
+  formProfissional = new FormGroup({
+    nome: new FormControl<string | null>(null),
+    registroConselho: new FormControl<string | null>(null),
+    especialidade_id: new FormControl<number | null>(null),
+    unidade_id: new FormControl<number | null>(null),
+    telefone: new FormControl<string | null>(null),
+    email: new FormControl<string | null>(null)
+  });
+
+  get form() {
+    return this.formProfissional.controls;
+  }
   
   salvar(): void {
+    this.registro = Object.assign(this.registro, this.formProfissional.value);
     this.servico.salvar(this.registro).subscribe({
       next: id => alert(`ID gerado: ${id}`),
       complete: () => {
